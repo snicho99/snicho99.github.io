a18dcd3478f77390c10b5067fd644011d3e9a242
Steven Nicholson <steven@airbag.co>
2024-07-18 09:44:30 +1000

    Initial Commit

---
+++ _nuxt/49ddmg0P.js
@@ -0,0 +1,1 @@
+import{U as o,l as u,V as f,v,y as d,W as l,X as i,Y as h,Z as m}from"./C-WV57N_.js";function I(t,a={}){const e=a.head||o();if(e)return e.ssr?e.push(t,a):p(e,t,a)}function p(t,a,e={}){const s=u(!1),n=u({});f(()=>{n.value=s.value?{}:m(a)});const r=t.push(n.value,e);return v(n,c=>{r.patch(c)}),h()&&(d(()=>{r.dispose()}),l(()=>{s.value=!0}),i(()=>{s.value=!1})),r}export{I as u};
